4423ae8faa1cd71d77e67639b7a38d81ee23949c
ccsbaa <57611957+ccsbaa@users.noreply.github.com>
2021-06-03 18:58:39 +0800

    Add files via upload

--- simplesheets-shopping-cart/js/main.js
+++ simplesheets-shopping-cart/js/main.js
@@ -16,7 +16,7 @@ jQuery(document).ready(function () {
 
 //* Define spreadsheet URL (make sure you add the #gid=0 for the sheet you want to use)
 //* var googleSheetURI = 'https://docs.google.com/spreadsheets/d/11BNDZeQ4nqwVA-BYViMeM1QfWBtqJT-hqKpc6hh22aM/edit#gid=0';
-var googleSheetURI = 'https://docs.google.com/spreadsheets/d/1_jtmni38-9eZBUCtHC8gtX405wPRhRSy_5TyTDecHe8/edit#gid=1804221129';
+var googleSheetURI = 'https://docs.google.com/spreadsheets/d/1Tl70IF6NJkT1LHSz_1rvJyioYQA1Mlr7pkmN9pqFn7Q/edit#gid=1348553966';
 //* Compile the Handlebars template for HR leaders
 var HRTemplate = Handlebars.compile($('#hr-template').html());
 
